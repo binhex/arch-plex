484f64adb4d41028d96e8267cc07cf1dc93803e4
binhex <megalith01@gmail.com>
2014-10-29 14:17:21 +0000

    additional notes and tidy up on values

--- plexmediaserver.sh
+++ plexmediaserver.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
+#the below is a consolidation of settings from (original) bash script /usr/bin/plexmediaserver.sh and environment file /etc/conf.d/plexmediaserver
 
-#set env variables
-export PLEX_MEDIA_SERVER_USER=nobody
-export PLEX_MEDIA_SERVER_HOME=/opt/plexmediaserver
-export PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR=/var/lib/plex
-export PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS=6
-export PLEX_MEDIA_SERVER_TMPDIR=/config/tmp
+#create tmp folder for transcodes
+mkdir -p /config/tmp
 
-#set env variables (replaces plexmediaserver.sh)
+#set env variables for plex
+export PLEX_MEDIA_SERVER_USER='nobody'
+export PLEX_MEDIA_SERVER_HOME='/opt/plexmediaserver'
+export PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR='/var/lib/plex'
+export PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS='6'
+export PLEX_MEDIA_SERVER_TMPDIR='/config/tmp'
+
+#set language variables (required for plex) must be same as locale set in base image
 export LANG='en_GB.UTF-8'
 export LC_ALL='en_GB.UTF-8'
-export LD_LIBRARY_PATH=/opt/plexmediaserver
-export TMPDIR=/config/tmp
 
-#set home directory (will auto create library files on start of "Plex Media Server")
-export HOME="/config/Library/Application Support"
+#this path allows the import of the other .so modules located in the /opt/plexmediaserver folder
+export LD_LIBRARY_PATH='/opt/plexmediaserver'
+
+#this specifies the folder to keep temporary transcodes in
+export TMPDIR='/config/tmp'
+
+#set home directory, this is where the library files are stored (auto created on run of Plex Media Server)
+export HOME='/config'
 
 #kick off main process
-exec /opt/plexmediaserver/Plex\ Media\ Server
-
-
+exec '/opt/plexmediaserver/Plex Media Server'
