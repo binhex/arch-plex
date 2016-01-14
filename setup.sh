5c76566b15646c172580cefd233e6c1fa39dc8be
binhex <megalith01@gmail.com>
2016-01-14 17:34:29 +0000

    enhance gitattributes file to support binarty artifact uploads

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-#config below is a consolidation of (original) bash script /usr/bin/plexmediaserver.sh and environment file /etc/conf.d/plexmediaserver
 
-#set env variables for plex
+# config below is a consolidation of (original) bash script /usr/bin/plexmediaserver.sh and environment file /etc/conf.d/plexmediaserver
+
+# set env variables for plex
 export PLEX_MEDIA_SERVER_USER='nobody'
 export PLEX_MEDIA_SERVER_HOME='/opt/plexmediaserver'
 export PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR='/config'
@@ -18,15 +19,15 @@ else
 	export TMPDIR="${TRANS_DIR}"
 fi
 
-#set language variables (required for plex) must be same as locale set in base image
+# set language variables (required for plex) must be same as locale set in base image
 export LANG='en_GB.UTF-8'
 export LC_ALL='en_GB.UTF-8'
 
-#this path allows the import of the .so library modules located in the /opt/plexmediaserver folder
+# this path allows the import of the .so library modules located in the /opt/plexmediaserver folder
 export LD_LIBRARY_PATH='/opt/plexmediaserver'
 
-#set home directory, this is where the library files are stored (auto created on run of Plex Media Server)
+# set home directory, this is where the library files are stored (auto created on run of Plex Media Server)
 export HOME='/config'
 
-#kick off main process
+# kick off main process
 exec '/opt/plexmediaserver/Plex Media Server'
